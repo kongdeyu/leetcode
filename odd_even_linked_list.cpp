3b621ea293a0acfb53bd6aec17bcc0d55c75dcac
kongdeyu <baidu@baidudeMacBook-Pro.local>
2016-02-17 19:20:25 +0800

    update odd_even_linked_list

--- odd_even_linked_list.cpp
+++ odd_even_linked_list.cpp
@@ -15,38 +15,17 @@ public:
         }
         
         ListNode *odd_lst = head;
-        ListNode *even_lst = NULL;
-        ListNode *cur_node = head->next;
+        ListNode *even_lst = head->next;
         ListNode *cur_odd_node = head;
-        ListNode *cur_even_node = NULL;
-        int32_t idx = 2;
-        while(cur_node != NULL)
+        ListNode *cur_even_node = head->next;
+        while(cur_even_node != NULL && cur_even_node->next != NULL)
         {
-            if(idx++ & 1)
-            {
-                cur_odd_node->next = cur_node;
-                cur_odd_node = cur_node;
-            }
-            else
-            {
-                if(even_lst == NULL)
-                {
-                    even_lst = cur_node;
-                    cur_even_node = cur_node;
-                }
-                else
-                {
-                    cur_even_node->next = cur_node;
-                    cur_even_node = cur_node;
-                }
-            }
-            cur_node = cur_node->next;
+            cur_odd_node->next = cur_even_node->next;
+            cur_odd_node = cur_odd_node->next;
+            cur_even_node->next = cur_even_node->next->next;
+            cur_even_node = cur_even_node->next;
         }
         cur_odd_node->next = even_lst;
-        if(cur_even_node != NULL)
-        {
-            cur_even_node->next = NULL;
-        }
         return head;
     }
 };
