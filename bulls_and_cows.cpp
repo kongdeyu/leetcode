12d2a3cd287897551b04b602d5ac1abc4ba164f7
kongdeyu <baidu@baidudeMacBook-Pro.local>
2016-03-03 15:34:02 +0800

    update bulls_and_cows

--- bulls_and_cows.cpp
+++ bulls_and_cows.cpp
@@ -6,34 +6,26 @@ public:
         int bulls = 0;
         int cows = 0;
         
-        std::vector<int> idr2secret(secret.size(), 0);
-        std::vector<int> idr2guess(guess.size(), 0);
-        
+        std::vector<int> count(128, 0);
         for(size_t idx = 0; idx < secret.size(); idx++)
         {
             if(secret[idx] == guess[idx])
             {
                 bulls++;
-                idr2secret[idx] = 1;
-                idr2guess[idx] = 1;
             }
-        }
-        
-        for(size_t idx2sec = 0; idx2sec < secret.size(); idx2sec++)
-        {
-            if(idr2secret[idx2sec] == 1)
+            else
             {
-                continue;
-            }
-            
-            for(size_t idx2gue = 0; idx2gue < guess.size(); idx2gue++)
-            {
-                if(idr2guess[idx2gue] == 0 && secret[idx2sec] == guess[idx2gue])
+                char ch = secret[idx];
+                count[ch]++;
+                if(count[ch] <= 0)
                 {
                     cows++;
-                    idr2secret[idx2sec] = 1;
-                    idr2guess[idx2gue] = 1;
-                    break;
+                }
+                ch = guess[idx];
+                count[ch]--;
+                if(count[ch] >= 0)
+                {
+                    cows++;
                 }
             }
         }
