3791cc0c5f074cabc02a7708f1071ac64543d1bd
kongdeyu <baidu@baidudeMacBook-Pro.local>
2016-03-07 13:37:18 +0800

    update product_of_array_except_self

--- product_of_array_except_self.cpp
+++ product_of_array_except_self.cpp
@@ -3,42 +3,19 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         assert(nums.size());
         
-        int n_zero_num = 0;
-        int product = 1;
-        for(std::vector<int>::const_iterator cit = nums.begin(); cit != nums.end(); cit++)
+        std::vector<int> res(nums.size(), 1);
+        for(int idx = 1; idx < nums.size(); idx++)
         {
-            if(*cit == 0)
-            {
-                n_zero_num++;
-            }
-            else
-            {
-                product *= *cit;
-            }
+            res[idx] = res[idx - 1] * nums[idx - 1];
         }
         
-        std::vector<int> res(nums.size(), 0);
-        if(n_zero_num > 1)
+        int temp = 1;
+        for(int idx = nums.size() - 2; idx >= 0; idx--)
         {
-            return res;
+            temp *=  nums[idx + 1];
+            res[idx] *= temp;
         }
-        else if(n_zero_num == 1)
-        {
-            for(size_t idx = 0; idx < nums.size(); idx++)
-            {
-                if(nums[idx] == 0)
-                {
-                    res[idx] = product;
-                }
-            }
-        }
-        else
-        {
-            for(size_t idx = 0; idx < nums.size(); idx++)
-            {
-                res[idx] = product / nums[idx];
-            }
-        }
+        
         return res;
     }
 };
