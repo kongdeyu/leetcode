edae83f9005b2978a975e9c50d595294ea3002e3
kongdeyu <baidu@baidudeMacBook-Pro.local>
2016-03-14 20:18:19 +0800

    update single_number_II

--- single_number_II.cpp
+++ single_number_II.cpp
@@ -3,20 +3,13 @@ public:
     int singleNumber(vector<int>& nums) {
         assert(nums.size());
         
-        int one_times = 0;
-        int two_times = 0;
+        int ones = 0, twos = 0;
         for(std::vector<int>::const_iterator cit = nums.begin(); cit != nums.end(); cit++)
         {
-            int temp = *cit;
-            int mask = two_times & temp;
-            two_times ^= mask;
-            temp ^= mask;
-            
-            two_times |= (temp & one_times);
-            one_times ^= temp;
+            ones = (ones ^ (*cit)) & ~twos;
+            twos = (twos ^ (*cit)) & ~ones;
         }
-        
-        return one_times | two_times;
+        return ones;
     }
     
 };
