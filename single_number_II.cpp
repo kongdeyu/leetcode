346cfa40ca0368a49a299610f8ae239780f604e1
kongdeyu <baidu@baidudeMacBook-Pro.local>
2016-03-15 10:46:38 +0800

    update single_number_II

--- single_number_II.cpp
+++ single_number_II.cpp
@@ -3,13 +3,25 @@ public:
     int singleNumber(vector<int>& nums) {
         assert(nums.size());
         
-        int ones = 0, twos = 0;
-        for(std::vector<int>::const_iterator cit = nums.begin(); cit != nums.end(); cit++)
+        int res = 0;
+        for(int i = 0; i < 8 * sizeof(int); i++)
         {
-            ones = (ones ^ (*cit)) & ~twos;
-            twos = (twos ^ (*cit)) & ~ones;
+            int mask = 1 << i;
+            int sum = 0;
+            for(std::vector<int>::const_iterator cit = nums.begin(); cit != nums.end(); cit++)
+            {
+                if((*cit) & mask)
+                {
+                    sum++;
+                }
+            }
+            
+            if(sum % 3)
+            {
+                res |= mask;
+            }
         }
-        return ones;
+        return res;
     }
     
 };
