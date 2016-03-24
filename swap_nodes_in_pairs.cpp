59749e71971d7aa935258a38de88a3d3654cbe8e
kongdeyu <baidu@baidudeMacBook-Pro.local>
2016-03-24 19:06:13 +0800

    update swap_nodes_in_pairs

--- swap_nodes_in_pairs.cpp
+++ swap_nodes_in_pairs.cpp
@@ -14,33 +14,11 @@ public:
             return head;
         }
         
-        ListNode *new_head = head->next;
-        ListNode *pre = head;
-        ListNode *cur = head->next;
-        while(true)
-        {
-            ListNode *next = cur->next;
-            if(next == NULL)
-            {
-                pre->next = NULL;
-                cur->next = pre;
-                break;
-            }
-            
-            if(next->next == NULL)
-            {
-                pre->next = next;
-                cur->next = pre;
-                break;
-            }
-            
-            pre->next = next->next;
-            cur->next = pre;
-            
-            cur = pre->next;
-            pre = next;
-        }
-        return new_head;
+        ListNode *new_head = head->next->next;
+        ListNode *cur_head = head->next;
+        head->next->next = head;
+        head->next = swapPairs(new_head);
+        return cur_head;
     }
     
 };
