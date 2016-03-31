e6b73ee5f7350235e35b1e6c787ed908173f03c4
kongdeyu <baidu@baidudeMacBook-Pro.local>
2016-03-31 17:16:45 +0800

    update unique_paths

--- unique_paths.cpp
+++ unique_paths.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         assert(m > 0 && m <=100 && n > 0 && n <=100);
-        
-        std::vector<int> vec_path(m * n, -1);
-        return unique_paths(m, n, m, n, vec_path);
-    }
-    
-private:
-    int unique_paths(int i, int j, int m, int n, std::vector<int> &vec_path)
-    {
-        if(i == 1 || j == 1)
+        if(m == 1 || n == 1)
         {
             return 1;
         }
         
-        if(vec_path[(i - 1) * n + j - 1] == -1)
+        std::vector<int> vec_path(n, 1);
+        for(int i = 1; i < m; i++)
         {
-            vec_path[(i - 1) * n + j - 1] = unique_paths(i - 1, j, m, n, vec_path) + unique_paths(i, j - 1, m, n, vec_path);
+            for(int j = 1; j < n; j++)
+            {
+                vec_path[j] += vec_path[j - 1];
+            }
         }
-        
-        return vec_path[(i - 1) * n + j - 1];
+        return vec_path[n - 1];
     }
     
 };
