fe6592f081615e2254ddf96b14c4bbf39c073514
kongdeyu <baidu@baidudeMacBook-Pro.local>
2016-05-19 19:07:52 +0800

    update integer_break

--- integer_break.cpp
+++ integer_break.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        if(n < 2)
-        {
-            return 0;
-        }
+        assert(n > 1);
         
-        if(_m_vec_max_product.size() < n)
+        int res = 0;
+        for(int i = 2; i <= n; i++)
         {
-            _m_vec_max_product.resize(n, 0);
-            _m_vec_max_product[1] = 1;
-        }
-        
-        if(_m_vec_max_product[n - 1] != 0)
-        {
-            return _m_vec_max_product[n - 1];
-        }
-        
-        int max_product = 0;
-        for(int i = 1; i <= n / 2; i++)
-        {
-            int left = std::max(i, integerBreak(i));
-            int right = std::max(n - i, integerBreak(n - i));
-            int cur_product = left * right;
-            if(cur_product > max_product)
+            int quotient = n / i;
+            int remainder = n % i;
+            int cur_product = pow(quotient, i - remainder) * pow(quotient + 1, remainder);
+            if(cur_product > res)
             {
-                max_product = cur_product;
+                res = cur_product;
             }
         }
-        _m_vec_max_product[n - 1] = max_product;
-        return max_product;
+        return res;
     }
-    
-private:
-    std::vector<int> _m_vec_max_product;
-    
 };
