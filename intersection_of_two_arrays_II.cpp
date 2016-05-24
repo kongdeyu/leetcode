a019ca4f29550e35366eaf462ef957ef6700f9d6
kongdeyu <baidu@baidudeMacBook-Pro.local>
2016-05-24 19:39:32 +0800

    update intersection_of_two_arrays_II

--- intersection_of_two_arrays_II.cpp
+++ intersection_of_two_arrays_II.cpp
@@ -7,23 +7,37 @@ public:
             return res;
         }
         
-        std::map<int, std::vector<int> > map_nums1;
+        std::map<int, int> map_nums1;
         for(std::vector<int>::const_iterator cit = nums1.begin(); cit != nums1.end(); cit++)
         {
-            map_nums1[*cit].push_back(*cit);
+            if(map_nums1.find(*cit) != map_nums1.end())
+            {
+                map_nums1[*cit]++;
+            }
+            else
+            {
+                map_nums1[*cit] = 1;
+            }
         }
         
-        std::map<int, std::vector<int> > map_nums2;
+        std::map<int, int> map_nums2;
         for(std::vector<int>::const_iterator cit = nums2.begin(); cit != nums2.end(); cit++)
         {
-            map_nums2[*cit].push_back(*cit);
+            if(map_nums2.find(*cit) != map_nums2.end())
+            {
+                map_nums2[*cit]++;
+            }
+            else
+            {
+                map_nums2[*cit] = 1;
+            }
         }
         
-        for(std::map<int, std::vector<int> >::const_iterator cit = map_nums1.begin(); cit != map_nums1.end(); cit++)
+        for(std::map<int, int>::const_iterator cit = map_nums1.begin(); cit != map_nums1.end(); cit++)
         {
             if(map_nums2.find(cit->first) != map_nums2.end())
             {
-                res.insert(res.end(), std::min(cit->second.size(), map_nums2[cit->first].size()), cit->first);
+                res.insert(res.end(), std::min(cit->second, map_nums2[cit->first]), cit->first);
             }
         }
         return res;
