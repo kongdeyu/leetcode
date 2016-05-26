c6b58bf56dc2c7c83672a18c4b6f36fee1b5ac47
kongdeyu <baidu@baidudeMacBook-Pro.local>
2016-05-26 21:04:06 +0800

    add different_ways_to_add_parentheses

---
+++ different_ways_to_add_parentheses.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string input) {
+        std::vector<int> res;
+        if(input.empty())
+        {
+            return res;
+        }
+        
+        for(std::string::iterator cit = input.begin(); cit != input.end(); cit++)
+        {
+            if(*cit == '+' || *cit == '-' || *cit == '*')
+            {
+                std::vector<int> left = diffWaysToCompute(std::string(input.begin(), cit));
+                std::vector<int> right = diffWaysToCompute(std::string(cit + 1, input.end()));
+                for(std::vector<int>::const_iterator cit2left = left.begin(); cit2left != left.end(); cit2left++)
+                {
+                    for(std::vector<int>::const_iterator cit2right = right.begin(); cit2right != right.end(); cit2right++)
+                    {
+                        res.push_back(calc(*cit2left, *cit2right, *cit));
+                    }
+                }
+            }
+        }
+        
+        if(res.empty())
+        {
+            int num = 0;
+            for(std::string::const_iterator cit = input.begin(); cit != input.end(); cit++)
+            {
+                int digit = *cit - '0';
+                num = num * 10 + digit;
+            }
+            res.push_back(num);
+        }
+        return res;
+    }
+    
+private:
+    int calc(int left, int right, char op)
+    {
+        if(op == '+')
+        {
+            return left + right;
+        }
+        if(op == '-')
+        {
+            return left - right;
+        }
+        if(op == '*')
+        {
+            return left * right;
+        }
+        return 0;
+    }
+    
+};
