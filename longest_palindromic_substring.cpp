937b3a0e98c91d6f0bb03b2dbe1b23da98c5b80e
kongdeyu <baidu@baidudeMacBook-Pro-4.local>
2018-03-27 19:25:29 +0800

    update longest_palindromic_substring

--- longest_palindromic_substring.cpp
+++ longest_palindromic_substring.cpp
@@ -6,28 +6,19 @@ public:
             return res;
         }
 
-        if (s.size() == 1) {
-            return s;
-        }
-
-        int cur_max_len = 1;
         res = std::string(s.begin(), s.begin() + 1);
         for (std::string::const_iterator cit = s.begin(); cit != s.end() - 1; cit++) {
             for (int shift = 0; shift < 2; shift++) {
                 std::string::const_iterator left = cit;
                 std::string::const_iterator right = cit + shift;
-                if (*left != *right) {
-                    break;
-                }
 
-                while (left != s.begin() && right != s.end() - 1 && *(left - 1) == *(right + 1)) {
+                while (left >= s.begin() && right <= s.end() - 1 && *left == *right) {
                     left--;
                     right++;
                 }
 
-                if (cur_max_len < right - left + 1) {
-                    cur_max_len = right - left + 1;
-                    res = std::string(left, right + 1);
+                if (res.length() < right - left - 1) {
+                    res = std::string(left + 1, right);
                 }
             }
         }
